9b604cf9a9b1e2bc85ee78b36797df8453df92df
PGCranford <PGCranford@gmail.com>
2022-08-16 16:08:12 -0400

    renamed models

--- models/index.js
+++ models/index.js
@@ -2,10 +2,10 @@ const Vehicle = require('./Vehicle');
 const Renter = require('./Renter');
 
 //Renter belongs to Vehicle
-// Renter.hasMany(Vehicle, {
-//     foreignKey: 'renter_id',
-//     onDelete: 'SET NULL'
-// });
+Renter.hasMany(Vehicle, {
+    foreignKey: 'renter_id',
+    onDelete: 'SET NULL'
+});
 
 //Vehicle belongs to Renter
 Vehicle.belongsTo(Renter, {
